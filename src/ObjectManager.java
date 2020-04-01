3e3bc75be5fc8e5fcc08817f4438c23f3fb5c2f1
timla <timla@leviathan>
2020-04-01 12:56:12 -0700

    Computer

---
+++ src/ObjectManager.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ObjectManager {
+	ArrayList<Button> buttons = new ArrayList<Button>();
+	
+	public ObjectManager() {
+		
+	}
+	
+	void addButton(Button button) {
+		buttons.add(button);
+	}
+	
+	void update() {
+		
+	}
+	
+	void draw() {
+		
+	}
+}
